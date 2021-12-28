62ae41e7557a800ad3d14bd7321c93d56e0f5bbb
felipepadua011 <89053801+felipepadua011@users.noreply.github.com>
2021-12-28 12:26:31 -0300

    add

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,34 +1,51 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-import { PrismaService } from '../prisma/prisma.service';
-import { Usuario, Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { Usuario } from '@prisma/client';
+import { LoginDto } from 'src/auth/dto/login.dto';
+import { JwtPayload } from 'src/auth/jwt.strategy';
 
 @Injectable()
 export class UsuariosService {
+  // eslint-disable-next-line prettier/prettier
   constructor(private prisma: PrismaService) {}
 
-  async create(data: Prisma.UsuarioCreateInput): Promise<Usuario> {
+  async create(data: CreateUsuarioDto): Promise<Usuario> {
     data.senha = await bcrypt.hash(data.senha, 10);
     return await this.prisma.usuario.create({ data });
   }
 
-  async findByLogin(login: CreateUsuarioDto): Promise<Usuario> {
+  async findByLogin(login: LoginDto): Promise<Usuario>{
     const user = await this.prisma.usuario.findFirst({
       where: {
         email: login.email,
       },
-    });
+    })
 
-    if (!user) {
-      throw new HttpException('Usuario não encontrado', HttpStatus.NOT_FOUND);
+    if(!user) {
+      throw new HttpException('usuario nao encontrado',HttpStatus.NOT_FOUND);
     }
 
     const senhaIgual = await bcrypt.compare(login.senha, user.senha);
 
     if (!senhaIgual) {
-      throw new HttpException('Senha invalida', HttpStatus.UNAUTHORIZED);
+      throw new HttpException('senha invalida', HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
+  }
+
+  async validateUser(payload: JwtPayload): Promise<Usuario> {
+    const user = await this.prisma.usuario.findFirst({
+      where: {
+        email: payload.email,
+      }
+    });
+
+    if(!user){
+      throw new HttpException('Token Invalido', HttpStatus.UNAUTHORIZED);
     }
 
     return user;
@@ -42,7 +59,8 @@ export class UsuariosService {
     return `This action returns a #${id} usuario`;
   }
 
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(id: number, _updateUsuarioDto: UpdateUsuarioDto) {
     return `This action updates a #${id} usuario`;
   }
 
